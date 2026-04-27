c0e8f5a2b1f5c5618487f75a5d825d10dbb80d60
Exequiel <exequiel.miranda245@gmail.com>
2026-04-27 07:23:43 -0600

    cors y login

--- backend/app.js
+++ backend/app.js
@@ -6,9 +6,19 @@ import customerRoutes from "./src/routes/customer.js";
 import registerCustomerRoutes from "./src/routes/registerCustomer.js";
 import cookieParser from "cookie-parser";
 import loginCustomerRoutes from "./src/routes/login.js";
+import logoutRoutes from "./src/routes/logout.js";
+import cors from "cors";
 
 //Creo una constante que guarde Express
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    // permitir el envío de cookies y credenciales
+    credentials: true,
+  }),
+);
 
 app.use(cookieParser());
 
@@ -21,5 +31,6 @@ app.use("/api/employees", employeesRoutes);
 app.use("/api/customers", customerRoutes);
 app.use("/api/registerCustomer", registerCustomerRoutes);
 app.use("/api/login", loginCustomerRoutes);
+app.use("/api/logout", logoutRoutes);
 
 export default app;
